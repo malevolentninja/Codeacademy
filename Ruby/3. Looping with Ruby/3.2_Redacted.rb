7cdc4238bd9060e3d0282c9c4679add6fde87047
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 19:52:17 +0000

    Create 3.2_Redacted.rb

---
+++ Ruby/3. Looping with Ruby/3.2_Redacted.rb
@@ -0,0 +1,16 @@
+# Simple program to change the user's input with arrays and iterators.
+
+puts "Text to search through: "
+text = gets.chomp
+puts "Word to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
+end
