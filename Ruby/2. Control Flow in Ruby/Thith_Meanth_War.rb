17f0031b6eab2dc494b85f94ec3780e99a1c99a0
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 19:03:42 +0000

    Create Thith_Meanth_War.rb

---
+++ Ruby/2. Control Flow in Ruby/Thith_Meanth_War.rb
@@ -0,0 +1,14 @@
+# combine control flow with new ruby string methods to transform a user's string to Daffy Duck's voice 
+# aka changing "s" into "th"
+
+print "Thtring, pleathe!: "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  puts "Nothing to do here!"
+end
+  
+puts "Your string is: #{user_input}"
