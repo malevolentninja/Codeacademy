063723ce5ba5c38e4c166418ec1fba5d36e90114
Olivia Boertje <malevolentninja@gmail.com>
2017-01-05 18:42:11 +0000

    Create Introduction_to_ruby.rb
    Lesson 1 - 16 in the First part of Introduction to Ruby

---
+++ Ruby/1. Introduction_to_ruby/Introduction_to_ruby.rb
@@ -0,0 +1,102 @@
+#Variables & Data Types
+
+# Welcome to Ruby!
+
+#2. Data Types: Numbers Strings, Booleans
+
+my_num =25    # Add your code here!
+
+my_boolean =true    # And here!
+
+my_string ="Ruby"    # Also here.
+
+# 3. Variables 
+
+my_num=100
+
+# 4. Math
+
+39 + 21
+78 - 8
+30 * 5
+16 / 4
+2 ** 3
+4 % 5
+
+# 5. 'puts' and 'print'
+
+puts "interesting test?"
+
+print "marco Polo"
+
+puts "This is only the start."
+print "I want to learn more!"
+
+# String Methods 
+
+# 6. Everything in Ruby is an Object
+#nothing to write 
+
+# 7. The '.length' Method
+"malevolentninja".length #15
+
+
+# 8. The '.reverse' Method
+"malevolentninja".reverse #"ajnintnelovelam" 
+
+# 9. '.upcase' & '.downcase'
+
+puts "malevolentninja".upcase # MALEVOLENTNINJA
+puts"malevolentninja".downcase # malevolentninja
+
+
+
+# Writing Good Code
+
+# 10. Single-line Comments
+# I like coffee!
+#"chocolate" is good.length
+
+# 11. Multi-line comments 
+
+=begin
+I'm commenting!
+Another one without the #
+=end
+
+
+# 12. Naming Conventions
+
+name= "malevolentninja" #"malevolentninja"
+
+# Review 
+#13. Variables & Data Types 
+
+my_name= "Malevolentninja"
+my_age = 18
+hungry = true
+
+
+# 14. Math
+
+sum= 13 + 379
+product= 923 * 15
+quotient = 13209 / 17
+
+# 15. Strings and String Methods
+name="malevolentninja"
+.downcase
+.upcase
+.reverse
+.upcase
+# "AJNINTNELOVELAM"
+
+
+# 16. Comments 
+#this is a start
+
+=begin
+this is a 
+multiline comment
+=end
+
