1222250315f6b1f076c25fc5fbe105cc35533617
Olivia Boertje <malevolentninja@gmail.com>
2017-01-06 11:12:14 +0000

    Create Hashes_and_Symbols.rb

---
+++ Ruby/6.Hashes/Hashes_and_Symbols.rb
@@ -0,0 +1,43 @@
+1. The Story So Far
+my_hash = { "name" => 1 }
+
+2. Iterating Over Hashes
+
+matz = { "First name" => "Yukihiro",
+  "Last name" => "Matsumoto",
+  "Age" => 47,
+  "Nationality" => "Japanese",
+  "Nickname" => "Matz"
+}
+
+matz.each do |key, value|
+  puts value, matz[value]
+end
+
+3. Nil: a Formal Introduction 
+
+
+
+4. Setting Your Own Default
+
+5. A Key of a Different Colour. 
+
+6. What's a Symbol?
+
+7. Symbol Syntax
+
+8. What are Symbols Used For?
+
+9. Coverting Between Symbols and Strings
+
+10. Many paths to the Same Summit 
+
+11. All Aboard the Hash Rocket!
+
+12. The Hash Rocket Has Landed 
+
+13. Dare to Compare
+
+14 Becoming More Selective
+
+15. More Methods , More Solutions
