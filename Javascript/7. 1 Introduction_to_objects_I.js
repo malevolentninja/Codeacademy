4135c3de6f29652643fb092be251a14713b9f51c
Olivia Boertje <malevolentninja@gmail.com>
2017-01-08 16:34:16 +0000

    Create 7. 1 Introduction_to_objects_I.js

---
+++ Javascript/7. 1 Introduction_to_objects_I.js
@@ -0,0 +1,62 @@
+1. We have come a long , long...
+2. Through the hard times...
+3....And the good!
+4. I have to celebrate you baby. 
+5. I have to praise
+6. Intro
+
+7. Properties 
+
+8. Accessing Properties 
+
+9. Accessing properties, Part 2
+
+10. Another Way to Create 
+
+11. Putting it all together 
+
+12. More practice Making objects
+
+13. Function Review 
+
+14. So What is A method? 
+
+15. Why are Methods Important? 
+
+16. The "this" Keyword
+
+17. "This" Works for everyone
+
+18. Make your own method 
+
+19. More kinds of methods 
+
+20. The object Contructor 
+
+21. Custom Constructors 
+
+22. Try it out! 
+
+23. More Options
+
+
+24. Constructors with methods
+
+25. Constructors in Review 
+
+26. Arrays of objects 
+
+27. Loop the Loop
+
+28. Passing Objects into Functions
+
+29. Try it out!
+
+30. What are Objects For?
+
+31. Properties
+
+32. Customizing constructors
+
+33. Methods
+
