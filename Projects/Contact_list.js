c2df995e02cda6a7547fb5f9468de4109142fb46
Olivia Boertje <malevolentninja@gmail.com>
2017-01-08 16:24:59 +0000

    Create Contact_list.js

---
+++ Projects/Contact_list.js
@@ -0,0 +1,31 @@
+xvar friends = {
+bill: {
+    firstName: "Bill",
+    lastName: "Gates",
+    number: "(542) 555-5555",
+    address: ['One Microsoft Way','Redmond','WA','98052']
+    },
+steve: {
+    firstName:"Steve",
+    lastName:"Jobs",
+    number: "(541) 555-5555",
+    address: ['Two Apple Way','Redmond','CA','95014']
+    }
+};
+
+var list = function(friends) {
+    for (key in friends) {
+        console.log(key);
+    };
+};
+
+var search = function(name) {
+    for (var key in friends) {
+        if(friends[key].firstName === name) {
+            console.log(friends[key]);
+            return friends[key];
+            };
+    };
+};
+
+search("Steve");
