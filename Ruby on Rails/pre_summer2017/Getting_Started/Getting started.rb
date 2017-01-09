eb2be67ebf81aa1347600a1948d5d1eb2c3d6426
Olivia Boertje <malevolentninja@gmail.com>
2017-01-09 17:36:36 +0000

    Create Getting started.rb

---
+++ Ruby on Rails/pre_summer2017/Getting_Started/Getting started.rb
@@ -0,0 +1,29 @@
+1. Hello Rails I 
+
+$ rails new MySite
+
+$ bundle install 
+
+$ rails server
+
+2. Hello Rails II
+
+#Nothing to write
+
+3. Controller 
+
+rails generate 
+controller Pages
+
+#open app/controllers/pages_controller.rb.
+
+#within class PagesController , add a method home:
+
+class PagesController < ApplicationController 
+
+  def home
+  end
+
+end
+
+4. 
