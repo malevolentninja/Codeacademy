b6c744471c1f63c7548114a5355d519e87ee6af4
Olivia Boertje <malevolentninja@gmail.com>
2017-01-10 08:04:35 +0000

    Update The_Zen_of_Ruby.rb

--- Ruby/7. Refactoring/The_Zen_of_Ruby.rb
+++ Ruby/7. Refactoring/The_Zen_of_Ruby.rb
@@ -1,1 +1,67 @@
-lorem
+# 1. Ruby is a delight
+
+ruby_is_eloquent = true
+ruby_is_ugly = false
+
+puts "Ruby is eloquent!" if ruby_is_eloquent
+puts "Ruby's not ugly!" unless ruby_is_ugly
+
+# 2. A simpler 'if'
+
+# Type your Ruby code below!
+puts "Hi!" if true
+
+# 3. The One-line Unless
+
+# Type your Ruby code below!
+puts "Hi!" unless false
+
+# 4. One Good Turn Deserves a Ternary 
+
+# Type your Ruby code below!
+puts true ? "its true" : "its false"
+
+# 5. When and Then: The Case Statement
+
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+case greeting
+    when 'English' then puts "Hello!"
+    when 'French' then puts "Bonjour!"
+    when 'German' then puts "Guten Tag!"
+    when 'Finnish' then puts "Haloo!"
+    else puts "I don't know that language!"
+end
+
+#6. Conditional Assignment
+
+favorite_book = nil
+puts favorite_book
+
+favorite_book ||= "Cat's Cradle"
+puts favorite_book
+
+favorite_book ||= "Why's (Poignant) Guide to Ruby"
+puts favorite_book
+
+favorite_book = "Why's (Poignant) Guide to Ruby"
+puts favorite_book
+      
+#7. Now You Try!
+    
+# Write your code on line 2!
+favorite_language ||= "Ruby"
+puts favorite_language
+    
+#8. Implicit Return 
+
+
+#9. Short-Circuit Evaluation 
+
+#10. The right tool for the job 
+
+#11. Up the Down Staircase
+
+# 12. 
