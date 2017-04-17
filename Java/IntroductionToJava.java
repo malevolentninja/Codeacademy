f6662a6ffa86ea4635d86d1fd518a3bae355c396
Olivia Boertje <malevolentninja@gmail.com>
2017-04-17 14:02:19 +0100

    Create IntroductionToJava.java

---
+++ Java/IntroductionToJava.java
@@ -0,0 +1,161 @@
+Introduction to Java
+
+// 1. What is your name?
+
+public class YourName {
+	public static void main(String[] args) {
+
+		System.out.println("Olivia");
+
+	}
+}
+
+
+// 2. DataTypes I : int
+
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(2876);
+
+	}
+}
+
+
+// 3. DataTypes II: boolean
+
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+// 4. DataTypes III: char
+
+public class DataTypesC {
+	public static void main(String[] args) {
+
+		System.out.println('L');
+
+	}
+}
+
+
+
+// 5. Variables 
+
+public class Variables {
+	public static void main(String[] args) {
+
+		int myNumber = 42;
+		boolean isFun = true; 
+		char movieRating = 'A'; 
+
+	}
+}
+
+// 6. Whitespace
+
+
+public class WhiteSpace {
+	public static void main(String[] args) {
+
+		boolean isFormatted = false;
+    System.out.println(isFormatted);
+
+	}
+}
+
+//7. Comments
+
+public class Comments {
+	public static void main(String[] args) {
+
+		//System.out.println("Noise!"); - Had to comment this out for the tutorial
+    
+		/* 
+     Hello , 
+     this is the multi-line comment!
+    */
+
+		
+	}
+}
+
+//8. Math: +, -, *, and /
+
+public class Arithmetic {
+	public static void main(String[] args) {
+
+		int myNumber = 34 * 98;
+		System.out.println(myNumber);
+
+	}
+}
+
+
+// 9. Math: % 
+
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 17 % 5;
+		System.out.println(myRemainder);
+
+	}
+} 
+
+
+// 10. Relational Operators
+public class RelationalOperators {
+	public static void main(String[] args) {
+
+		System.out.println(9 > 12);
+
+	}
+}
+
+
+
+// 11. Equality Operators 
+
+public class EqualityOperators {
+	public static void main(String[] args) {
+
+    char myChar = 'L';
+    int myInt = -3;
+		System.out.println(myChar == myInt);
+
+	}
+}
+
+// 12. Generalizations
+
+public class Generalizations {
+	public static void main(String[] args) {
+
+
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;		
+		System.out.println(epicLevel);
+    
+		// single line comment
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
