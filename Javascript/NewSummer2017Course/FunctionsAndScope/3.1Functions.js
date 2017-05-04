b410aec5996e4d1d7d4fb47c672242288ffbf89f
Olivia Boertje <malevolentninja@gmail.com>
2017-05-04 15:13:41 +0100

    Update 3.1Functions.js

--- Javascript/NewSummer2017Course/FunctionsAndScope/3.1Functions.js
+++ Javascript/NewSummer2017Course/FunctionsAndScope/3.1Functions.js
@@ -1,1 +1,29 @@
 
+//1. Introduction to Functions
+//no code to write.
+
+//2. Functions 
+
+function takeOrder() {
+  console.log('Order pizza');
+}
+console.log(takeOrder);
+
+
+//3. Parameters 
+
+function takeOrder(topping) {
+  console.log('Order pizza topped with ' + topping);
+  takeOrder('bacon');
+}
+console.log(takeOrder);
+
+//4. Parameters II
+
+
+
+//5. return 
+
+//6. return II
+
+//7. Review Functions
