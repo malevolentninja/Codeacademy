fdafdb84edd59b915d4fe77ec0ab2724df5989b9
Olivia Boertje <malevolentninja@gmail.com>
2017-05-04 13:58:14 +0100

    Create Variables.js

---
+++ Javascript/NewSummer2017Course/IntroductionToJavascript/Variables.js
@@ -0,0 +1,49 @@
+
+
+// 1. Variables 
+
+// No code to write. 
+
+// 2. Create a Variable 
+
+var strength = '50,000 pounds';
+console.log('How much stuff can a variable hold?', strength);
+
+// 3. Create a Variable II
+
+var strength = '50,000 pounds';
+console.log('How much stuff can a variable hold?', strength);
+// added age and pet 
+var age = 22; // number
+console.log(age);
+
+var hasPet = true; // boolean value
+console.log(hasPet);
+
+
+// 4. Changing a Variable's Value 
+
+var morningAlarm = '6:30AM';
+morningAlarm = '7:00AM';
+console.log('Morning alarm is set to: ', morningAlarm);
+
+// 5. String Interpolation 
+
+
+var favoriteAnimal = 'flying bison';
+console.log('My favoriteAnimal: ' + favoriteAnimal );
+
+/* english version 
+
+var favouriteAnimal = 'flying bison';
+console.log('My favouriteAnimal: ' + favouriteAnimal);
+*/
+
+// 6. Review Variables 
+
+/*
+How to create variables.
+How to change a variable's value.
+How to interpolate, or insert, a variable into a string.
+
+*/
