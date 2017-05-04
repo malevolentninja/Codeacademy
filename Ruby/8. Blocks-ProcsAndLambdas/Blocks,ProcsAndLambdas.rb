4a667772ede98e4b393c01d9639f5968393dbb88
Olivia Boertje <malevolentninja@gmail.com>
2017-05-04 17:41:43 +0100

    Create Blocks,ProcsAndLambdas.rb

---
+++ Ruby/8. Blocks-ProcsAndLambdas/Blocks,ProcsAndLambdas.rb
@@ -0,0 +1,21 @@
+# 1. You Know this! 
+# 2. Collect 'Em All
+# 3. Learning to Yield
+# 4. Yielding With Parameters
+# 5. Try It Yourself!
+# 6. Keeping Your Code DRY
+# 7. Proc Syntax
+# 8. Why Procs?
+# 9. Creat Your Own!
+# 10. Call Me Maybe 
+# 11. Symbols, Meet Procs
+# 12. The Ruby Lambda 
+# 13. Lambda Syntax 
+# 14. Lambdas vs. Procs
+# 15. Now You Try!
+# 16. Quick Review 
+# 17. Been Around the Block a Few Times 
+# 18. Creating a Proc
+# 19. Passing Your Proc to a Method 
+# 20. Creating a Lambda 
+# 21 Passing Your Lambda to a Method
