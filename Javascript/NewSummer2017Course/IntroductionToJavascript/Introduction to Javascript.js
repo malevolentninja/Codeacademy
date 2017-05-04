30625bb6838bf7108f96e12b9f9d8d8999c354ca
Olivia Boertje <malevolentninja@gmail.com>
2017-05-04 13:46:51 +0100

    Create Introduction to Javascript.js

---
+++ Javascript/NewSummer2017Course/IntroductionToJavascript/Introduction to Javascript.js
@@ -0,0 +1,74 @@
+//1. Introduction
+// no code 
+
+//2. Types 
+
+var myString = "blondie";
+var myNumber = 7;
+var myBoolean = false;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+
+
+//3. Console 
+// Console log favourite pizza toppings 
+
+console.log('spinach' , 'egg', 'and olives.');
+
+
+//4. Maths Operators 
+
+console.log(3.5 + 22); // add 3.5 to age
+console.log(2017 - 1969); // the current year minus the year of the moonlanding 
+console.log(65 / 240); // 65 divided by 240
+console.log(0.2708333333333333 * 100); the answer to the previous console.log multiply by 100
+
+/* answers in order
+25.5
+48
+0.2708333333333333
+27.083333333333332
+*/
+
+//5. Math Operators II
+
+// phases of the moon
+console.log(365 / 27); // 13.518518518518519
+console.log(365 % 27); //14 by using the modulous able to work out exact
+
+
+//6. Random 
+
+console.log(Math.random() * 100); //11.55676433520263
+console.log( Math.floor(Math.random() * 100) ); //45
+
+//7. Commments 
+
+// Opening Line
+console.log('It was love at first sight.');
+
+/*console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+
+//8. Review Types and Operators 
+
+/* 
+
+Learnt Three data types: strings, numbers and booleans
+Math operators (+ - / *)
+Remainder after dividing using % (modulo) 
+Math.random - generates random number 
+Math.floor - make the random number a whole number 
+comments both single and multi-line
+
+*/
