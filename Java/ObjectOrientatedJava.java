170ae685326f03c271f3ce7ed2fb1dab913f0993
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-07-03 19:08:00 +0100

    Update ObjectOrientatedJava.java

--- Java/ObjectOrientatedJava.java
+++ Java/ObjectOrientatedJava.java
@@ -212,4 +212,24 @@ class Dog extends Animal{
 
 
 // 13. Generalizations
+class Coffee extends Beverage {
+	
+public Coffee() {
+
+}
+
+public void addSugar(int cubes) {
+
+    System.out.println("You added " + cubes + " sugar cubes.");
+
+}
+
+public static void main(String[] args) {
+
+Coffee myOrder = new Coffee();
+  myOrder.addSugar(2);
+  myOrder.isFull();
+}
+}
+}
 
