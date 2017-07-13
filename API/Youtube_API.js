2312fd1f3dc44cfa9943d8fb3fe4f37f9e293cae
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-07-13 09:59:08 +0100

    Update Youtube_API.js

--- API/Youtube_API.js
+++ API/Youtube_API.js
@@ -9,9 +9,9 @@ console.log(xhr.statusText);
 
 2. The Client/Server Relationship 
 
-						              	//////////
-							              //Server//
-						              	//////////
+			 //////////
+			  //Server//
+			   //////////
                       //    //////////       \\
                     //      //////////         \\
                   //            ||               \\
@@ -24,7 +24,7 @@ console.log(xhr.statusText);
                   ////////// ////////// //////////
                   
 3. No REST for the Wicked 
-// REST = REpresentation State Transfer //
+// REST = REpresentation State Transfer 
 // You (the client) make an HTTP request. What sends the response?
 
 // A: The Internet
@@ -35,7 +35,7 @@ console.log(xhr.statusText);
 var answer = 'C'
 
 4. A RESTful API
-// Example from API - format JSON //
+// Example from API - format JSON 
 
 // {
 //   "myBlog": {
@@ -47,8 +47,8 @@ var answer = 'C'
 // }
 
 5. Makring a Request 
-// xhr variable = XML HTTP Request //
-// console.log xhr.status and xhr.statusText to send through request //
+// xhr variable = XML HTTP Request 
+// console.log xhr.status and xhr.statusText to send through request 
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.codecademy.com/", false);
@@ -57,7 +57,7 @@ xhr.send();
 console.log(xhr.status);
 console.log(xhr.statusText);
 
-// Answer 200 OK //
+// Answer 200 OK
 
 6. The Four Verbs 
 /*  HTTP core methods
@@ -66,6 +66,128 @@ POST: sends new information to the specified source.
 PUT: updates existing information of the specified source.
 DELETE: removes existing information from the specified source. */ 
 
-// no code to write. //
+// no code to write. 
 
+7. Anatomy of a Request
+// POST /codecademy/learn-http HTTP/1.1
+// Host: www.codecademy.com
+// Content-Type: text/html; charset=UTF-8
 
+// Name=Eric&Age=26
+
+8. EndPoints
+// What are the four commonly used HTTP methods ("verbs")?
+
+// A: FIND, SEND, UPDATE, REMOVE
+// B: CREATE, READ, UPDATE, DELETE
+// C: GET, SEND, PUT, DELETE
+// D: GET, POST, PUT, DELETE
+
+var answer = 'D'
+
+9. Authentication & API Keys
+
+// Example API key 
+var apiKey = "FtHwuH8w1RDjQpOr0y0gF3AWm8sRsRzncK3hHh9";
+
+10. HTTP Status Codes
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/doesnt_exist", false);
+xhr.send();
+// Add your code below!
+console.log(xhr.status);
+// Answer: 404
+
+11. Anatomy of a Response 
+/* HTTP response structure: 
+A response line, which includes the three-digit HTTP status code;
+A header, which includes further information about the server and its response;
+The body, which contains the text of the response.
+*/
+
+// HTTP/1.1 200 OK
+// Content-Type: text/xml; charset=UTF-8
+
+// <?xml version="1.0" encoding="utf-8"?>
+// <string xmlns="https://www.codecademy.com/">Accepted</string>
+
+12. Parsing XML
+// XML =  Xtensible Markup Language
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.codecademy.com/files/samples/javascript_learn_apis.xml", false);
+
+xhr.setRequestHeader('Content-Type', 'text/xml');
+xhr.send();
+
+xmlDocument = xhr.responseXML;
+console.log(xmlDocument.childNodes['0'].textContent);
+
+13. Parsing JSON
+// JSON = Java Script Object Notation
+
+var demo = '{"pets": { "name": "Jeffrey", "species": "Giraffe"}}';
+
+var json = JSON.parse(demo);
+console.log(json);
+
+14. XML or JSON?
+// to know the type of data an API will send is via documentation!
+// code
+	
+15. REST Constraints and Requirements
+
+// 1. HTTP is a protocol that connects clients and ______.
+var answer1 = 'Server'
+
+// 2. The four HTTP methods are GET, POST, ___, and DELETE.
+var answer2 = 'PUT'
+
+// 3. A ___ error means the server goofed up. (200, 300, 400, or 500)
+var answer3 = 404
+
+16. Requests
+
+// Add your code below this line!
+xhr = new XMLHttpRequest();
+xhr.open("GET","https://www.codecademy.com/", false);
+xhr.send();
+
+// Add your code above this line!
+
+console.log(xhr.status);
+console.log(xhr.statusText);
+
+17. Authentication and API Keys
+
+// What's an API key?
+
+// A: An alphanumeric string used to identify you to an API
+// B: An OAuth token
+// C: An All-Purpose Internet key
+// D: The tool used to unlock an API gate
+
+var answer = 'A'
+
+18. XML and JSON
+
+// What data format is shown below? Set answer
+// equal to 'XML' for XML and 'JSON' for JSON.
+
+// {
+//   "Cartoon Foxes": {
+//     {
+//       "Name": "Fox Tall",
+//       "Job": "Bein' tall"
+//     },
+//     {
+//       "Name": "Fox Small",
+//       "Job": "Bein' small"
+//     }
+//   }
+// }
+
+var answer = 'JSON'
+
+19. You Did It! 
+// basics of HTTP and REST complete
